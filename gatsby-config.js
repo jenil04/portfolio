b61af7a964fdfe476566145207ba910cf25f67ce
Jenil Thakker <jenilkaushik.thakker@sjsu.edu>
2020-02-08 12:50:06 -0800

    refactoring website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,69 +1,70 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Jenil Thakker`,
-    description: `My personal website`,
-    author: `Jenil Thakker`,
-    twitter: `@jenil_thakker`,
-    email: `jenilkthakker@gmail.com`,
-    siteUrl: `https://jenilthakker.com/`,
-    gitUrl: `https://github.com/jenil04`
+    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
-    },
-    `gatsby-plugin-catch-links`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        useMozJpeg: true,
-        stripMetadata: true,
-        defaultQuality: 50,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
+        navigation: [
           {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 900,
-              quality: 75,
-              withWebp: true,
-              linkImagesToOriginal: false,
-            },
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/lekoarts_de`,
+          },
+          {
+            name: `Instagram`,
+            url: `https://www.instagram.com/lekoarts.de/`,
           },
         ],
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Lewis Gatsby Starter Blog`,
-        short_name: `LGSB`,
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
         start_url: `/`,
-        background_color: `#fcfcfc`,
-        theme_color: `#fcfcfc`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
         display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
   ],
-};
+}
--- src/fonts/index.js
+++
@@ -1,4 +0,0 @@
-import chivov11latin300 from './chivo-v11-latin-300.woff2';
-import chivov11latin700 from './chivo-v11-latin-700.woff2';
-
-export { chivov11latin300, chivov11latin700 };
