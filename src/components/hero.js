832addebcb2474d4bba49988daaddade6a913138
Jenil Thakker <jenilkthakker@gmail.com>
2020-07-31 13:06:21 +0530

    change footer

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,10 +8,11 @@ function Hero() {
                 <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900 font-soehne">Product Design, Crypto & Finance
         </h1>
-                    <h2 class="mb-8 leading-relaxed text-lg text-gray-800">I am a full-stack developer based out of San Francisco (Currently in India). For potential collaborations, feel free
+                    <h2 class="mb-8 leading-relaxed text-lg text-gray-800">I am a full-stack developer based out of San Francisco (Currently in India). For collaborations, feel free
                     to reach out to me.</h2>
                     <div class="flex justify-center"> 
-                        <a href="https://twitter.com/jenilkr"><button class="inline-flex text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 rounded-full">Say 👋</button></a>
+                        <a href="mailto:jenilkthakker@gmail.com"><button class="inline-flex text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 rounded-full">Say 👋</button></a>
+                        <a href="https://jenil.substack.com"><button class="ml-4 inline-flex text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 rounded-full">Newsletter 👉</button></a>
                     </div>
                 </div>
                 <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
