94eb35dca9728d0cf6c1a3103f9261844160a473
Jenil Thakker <jenilkthakker@gmail.com>
2020-09-10 12:58:56 +0530

    fix email

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,7 @@ function ContactPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Contact"
       />
-      <section class="text-gray-700 bg-gray-200 body-font" style={{backgroundImage: `url(${bg})`}}>
+      <section class="text-gray-700 bg-gray-200 body-font" style={{backgroundImage: `url(${bg})`, backgroundSize: "cover"}}>
         <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
           <img class="mb-10 object-cover object-center rounded" alt="hero" height='250px' width='250px' src={hero} />
           <div class="text-center lg:w-2/3 w-full">
@@ -19,7 +19,7 @@ function ContactPage() {
             <p class="mb-8 leading-relaxed font-machina text-lg text-gray-800">Feel free to reach out to me for work opportunities or collab on a side-project. I'm also looking to build a team (founding engineer) for a startup I'm working on, so if you are based in India, please 
             send me an email. Other than that, I am always looking to have intellectual conversations on tech, life or just about anything else.</p>
             <div class="flex justify-center">
-              <button href="mailto:jenilkthakker@gmail.com" class="inline-flex text-white hover:bg-white hover:text-black border border-black bg-black border-0 py-3 px-6 focus:outline-none shadow-lg rounded-full text-lg font-machina">Email me</button>
+              <a href="mailto:jenilkthakker@gmail.com"><button class="inline-flex text-white hover:bg-white hover:text-black border border-black bg-black border-0 py-3 px-6 focus:outline-none shadow-lg rounded-full text-lg font-machina">Email me</button></a>
             </div>
           </div>
         </div>
