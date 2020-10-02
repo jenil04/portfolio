1a3a50f0154c63abdc157862969b55e288d8cd90
Jenil Thakker <jenilkthakker@gmail.com>
2020-10-02 15:55:40 +0530

    cahnge urth

--- src/components/startup.js
+++ src/components/startup.js
@@ -1,9 +1,9 @@
 import React from "react";
-import urth from "./images/Urth.svg";
+import urth from "./images/Urth.png";
 
 function Startup() {
     return (
-        <section id="startup">
+        <section id="startup"> 
             <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-20">
                     <h1 class="sm:text-3xl text-2xl font-medium title-font text-black font-soehne">Current <span class="text-red-600">work</span></h1>
@@ -16,7 +16,7 @@ function Startup() {
                             <img alt="content" class="object-cover object-center h-full w-full" src={urth}/>
                         </div>
                         <h2 class="title-font text-2xl font-medium text-black mt-6 mb-3 font-soehne">Urth</h2>
-                        <p class="leading-relaxed text-base text-lg font-machina">Urth offers therapy online, with 1:1 sessions or group sessions, with guided meditation. Our unique iterative process involves consultations with a neurologist, psychologist and a psychiatrist.</p>
+                        <p class="leading-relaxed text-base text-lg font-machina">An online, on-demand, specialized therapy with our team of clinical psychologists, psychiatrists and neurologists.</p>
                         <a href="https://urth.club"><button class="flex mx-auto mt-6 text-white bg-black border-0 py-2 px-5 focus:outline-none hover:bg-white hover:text-black border border-black rounded-full font-machina shadow-lg">Learn more</button></a>
                     </div>
                 </div>
