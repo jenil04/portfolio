e208423618eef271f6fe04d4b893ca11adbc456f
Jenil Thakker <jenilkthakker@gmail.com>
2021-08-07 23:26:19 +0530

    adds updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "65f4c1b92e2816f09119";
+/******/ 	var hotCurrentHash = "3c5468070ce112d2adcd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,9 +11,9 @@ export default function Hero(): JSX.Element {
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black font-monob">Hi, I'm Jenil 
       </h1>
-          <p className="mb-8 leading-relaxed text-black text-xl">I like to build software that contributes towards human progress. I'm currently working on <Link href="https://coinvise.co"><a className="underline font-monob">Coinvise</a></Link>, the first decetralized social network on Ethereum. Occasionally, I like to write about crypto <Link href="https://jenil.substack.com"><a className="underline font-monob">here</a></Link>.</p>
-          <p className="leading-relaxed text-black text-xl mb-8">We're hiring developers, content creators, community/ops and design roles at Coinvise, reach out to me below if you're interested.</p>
-          <p className="leading-relaxed text-black text-xl">You'll be joining a VC backed startup, with competitive pay and freedom from work timings.</p>
+          <p className="mb-8 leading-relaxed text-black text-xl">I like to build software that contributes towards human progress. I'm currently working on <Link href="https://coinvise.co"><a className="underline font-monob">Coinvise</a></Link>, infrastructure for web3 tokenized creators & communities. I like to write about crypto <Link href="https://jenil.substack.com"><a className="underline font-monob">here</a></Link>.</p>
+          <p className="leading-relaxed text-black text-xl mb-8">I also invest in DAOs. If you're building one, feel free to reach out to me on <Link href="https://twitter.com/0xjenil"><a className="underline font-monob">twitter</a></Link>.</p>
+          <p className="leading-relaxed text-black text-xl">We're hiring senior fullstack devs & smart contract developers at Coinvise. If you'd like to join our team, reach out to me below.</p>
         </div>
       </div>
     </section>
